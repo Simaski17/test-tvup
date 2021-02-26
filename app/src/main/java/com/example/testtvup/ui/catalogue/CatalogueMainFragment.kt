122f36e0f2325b42e877de9cd2e7992969028f5f
Jimmy Hernandez <jimmyhernandezu@gmail.com>
2021-02-25 23:21:00 -0300

    added new catalogue fragment detail

--- app/src/main/java/com/example/testtvup/ui/catalogue/CatalogueMainFragment.kt
+++ app/src/main/java/com/example/testtvup/ui/catalogue/CatalogueMainFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -135,6 +136,11 @@ class CatalogueMainFragment : Fragment() {
 
     fun horizontalImageClick(data : HorizontalImageModel){
         Toast.makeText(activity?.applicationContext,"HORIZONTAL ${data.Image}", Toast.LENGTH_SHORT).show()
+//        val action = HomeFragmentDirections.actionHomeFragmentToIndicatorDetailFragment(code = it.codigo)
+//        findNavController().navigate(action)
+
+
+        
     }
 
     private fun showFeedItems(recyclerView: RecyclerView, list: ArrayList<Any>?) {
