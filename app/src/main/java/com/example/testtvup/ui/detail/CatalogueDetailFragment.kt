2d25a1e96e4d7a2d0be007b64ba3e7146cee7429
Jimmy Hernandez <jimmyhernandezu@gmail.com>
2021-02-26 02:14:01 -0300

    added design and data within detail view

--- app/src/main/java/com/example/testtvup/ui/detail/CatalogueDetailFragment.kt
+++ app/src/main/java/com/example/testtvup/ui/detail/CatalogueDetailFragment.kt
@@ -8,10 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.domain.Item
 import com.example.domain.ResponseIMDB
 import com.example.falabellatest.ui.common.*
 import com.example.testtvup.R
+import kotlinx.android.synthetic.main.fragment_catalogue_detail.*
+import kotlinx.android.synthetic.main.fragment_catalogue_main.*
 
 
 class CatalogueDetailFragment : Fragment() {
@@ -49,7 +52,18 @@ class CatalogueDetailFragment : Fragment() {
 
             data.notNull {
 
-                Log.e("QWERTY", "QWERTY $it ")
+                tvTitlle.text = it.title
+                tvDescription.text = it.description
+                tvYear.text = it.year.toString()
+                tvDirector.text = it.director
+
+                activity?.let { it1 ->
+                    Glide
+                        .with(it1.applicationContext)
+                        .load(it.art)
+                        .centerCrop()
+                        .into(ivCoverPage)
+                }
 
             }
 
