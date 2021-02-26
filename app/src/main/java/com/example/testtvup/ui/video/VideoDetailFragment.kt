c6459bf41241ab2a31a2a6ca11646aff4ddbb61f
Jimmy Hernandez <jimmyhernandezu@gmail.com>
2021-02-26 09:53:47 -0300

    finished changes in video video

--- app/src/main/java/com/example/testtvup/ui/video/VideoDetailFragment.kt
+++ app/src/main/java/com/example/testtvup/ui/video/VideoDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.testtvup.ui.video
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,12 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.example.testtvup.R
-import com.example.testtvup.ui.detail.CatalogueDetailFragmentArgs
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import kotlinx.android.synthetic.main.fragment_video_detail.*
 
 
 class VideoDetailFragment : Fragment() {
 
     private val args: VideoDetailFragmentArgs by navArgs()
+    var videoPlayer = activity?.let { SimpleExoPlayer.Builder(it.applicationContext).build() }
+    private var sampleUrl = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +34,40 @@ class VideoDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sampleUrl = args.url
+        initializePlayer()
+    }
+
+    private fun buildMediaSource(): MediaSource? {
+        val dataSourceFactory = DefaultDataSourceFactory(activity?.applicationContext, "sample")
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(Uri.parse(sampleUrl))
+    }
+
+    private fun initializePlayer() {
+        videoPlayer = activity?.let { SimpleExoPlayer.Builder(it.applicationContext).build() }
+        playerView?.player = videoPlayer
+       // progressBar.visibility = View.VISIBLE
+        buildMediaSource()?.let {
+            videoPlayer?.prepare(it)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        videoPlayer?.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        videoPlayer?.playWhenReady = false
+
+            releasePlayer()
+
+    }
+
+    private fun releasePlayer() {
+        videoPlayer?.release()
     }
 
 }
